fe3c9d178b521c4014e451987ecdf9ef4bf83f60
Pedanticc <a.m.sakhravi@gmail.com>
2023-09-13 01:11:53 +0330

    Basic structure of the Password Generator

---
+++ Logic/Vendor.js
@@ -0,0 +1,5 @@
+const option_1 = document.getElementById("8-char")
+const option_2 = document.getElementById("12-char")
+const option_3 = document.getElementById("16-char")
+// -------------
+const out_put_result = document.getElementById("out-put")
